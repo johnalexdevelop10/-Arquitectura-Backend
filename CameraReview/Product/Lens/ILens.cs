c6fdf661ce2d3ef9c818180775c16ab6c7f9b20e
Johndev <johnzeta10@gmail.com>
2023-08-21 19:58:20 -0500

    first

---
+++ CameraReview/Product/Lens/ILens.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CameraReview.Product.Lens
+{
+    public interface ILens : IProduct //aqui extiende las prop/ en comun de Iprodcut a lens 
+    {
+        public string Name { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string SKU { get; set; }
+    }
+}
